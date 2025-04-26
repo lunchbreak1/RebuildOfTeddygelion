6778f57a41020abe90481efe58ef4c42c04cde4c
lunchbreak1 <iank1266@gmail.com>
2025-04-26 01:40:02 -0400

    Added idle, reverse animations. Spin the back wheels

--- Assets/Scripts/WheelchairController.cs
+++ Assets/Scripts/WheelchairController.cs
@@ -17,6 +17,13 @@ public class WheelchairController : MonoBehaviour
     [Tooltip("Player input")]
     public Vector2 moveDirection;
 
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
     /// <summary>
     /// This event is called when the player lands on the ground.
     /// </summary>
@@ -44,6 +51,8 @@ public class WheelchairController : MonoBehaviour
 
         // Normalize the direction so it's always of unit length (magnitude of 1)
         moveDirection.Normalize();
+
+        Animate(horizontal, vertical);
     }
 
     void FixedUpdate()
@@ -55,6 +64,12 @@ public class WheelchairController : MonoBehaviour
 
             transform.RotateAround(transform.position, transform.right, moveDirection.y * turnSpeed);
         }
+    }
+
+    void Animate(float horizontal, float vertical)
+    {
+        anim.SetFloat("Horizontal", horizontal);
+        anim.SetFloat("Vertical", vertical);
     }
 
 
