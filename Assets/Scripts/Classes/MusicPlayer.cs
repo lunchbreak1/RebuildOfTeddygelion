bf53bbd8e15da6c0381302c392c961b9b965f81f
lunchbreak1 <iank1266@gmail.com>
2026-07-09 08:11:31 -0400

    Added looping music

---
+++ Assets/Scripts/Classes/MusicPlayer.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    private static MusicPlayer instance;
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+}
